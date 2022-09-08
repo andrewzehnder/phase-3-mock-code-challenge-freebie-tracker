d75b88216ec712bedc29494435b72dda90b5efde
Andrew <azehnder2@gmail.com>
2022-09-08 14:48:05 -0700

    Submitting

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,23 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        item = self.freebies.where(item_name: item_name)
+        if item.count > 0
+            true
+        else
+            false
+        end
+    end
+
+    def give_away(dev, freebie)
+        if freebie.dev_id == self.id
+            freebie.dev_id = dev.id
+            freebie.save
+        else
+            "Sorry"
+        end
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,61 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+#Dev.all[rand(1..Dev.all.length)].id to get a random Dev
+
+Freebie.create(item_name: "Shirt", 
+value: 10, 
+dev_id: rand(1..4), 
+company_id: rand(1..4))
+
+Freebie.create(item_name: "Shirt", 
+value: 10, 
+dev_id: 3, 
+company_id: 1)
+
+Freebie.create(item_name: "Shirt", 
+value: 10, 
+dev_id: 4, 
+company_id: 1)
+
+Freebie.create(item_name: "Backpack", 
+value: 20, 
+dev_id: 1, 
+company_id: 2)
+
+Freebie.create(item_name: "Backpack", 
+value: 20, 
+dev_id: 2, 
+company_id: 2)
+
+Freebie.create(item_name: "Backpack", 
+value: 20, 
+dev_id: 3, 
+company_id: 2)
+
+Freebie.create(item_name: "Backpack", 
+value: 20, 
+dev_id: 4, 
+company_id: 2)
+
+Freebie.create(item_name: "Pen", 
+value: 2, 
+dev_id: 1, 
+company_id: 4)
+
+Freebie.create(item_name: "Pen", 
+value: 2, 
+dev_id: 1, 
+company_id: 3)
+
+Freebie.create(item_name: "Pen", 
+value: 2, 
+dev_id: 2, 
+company_id: 4)
+
+Freebie.create(item_name: "Pen", 
+value: 2, 
+dev_id: 4, 
+company_id: 1)
 
 puts "Seeding done!"
